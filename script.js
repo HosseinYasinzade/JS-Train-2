9ba4ae5d5d41e53008165c915e6d487e1f869c22
Hossein <hosseinyasinzadeh2002@gmail.com>
2024-09-14 08:24:22 +0330

    write rectange area func

---
+++ script.js
@@ -0,0 +1,10 @@
+// =============== rectangle area ==============================
+
+// const length = +prompt("Please enter length : ");
+// const width = +prompt("Please enter width : ");
+
+// const calculateArea = (length, widtth) => length * widtth;
+
+// console.log(calculateArea(length, width));
+
+// ========================================================
